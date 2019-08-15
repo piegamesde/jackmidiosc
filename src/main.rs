e0594b5f73ac4c128d59ce9877e25d2279b529fa
Piegames <14054505+piegamesde@users.noreply.github.com>
2019-08-15 22:57:08 +0200

    Basic logging and error handling
    Results in the `ProcessHandler` are still `unwrap`ped

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use std::net::UdpSocket;
 use std::sync::mpsc;
 
+#[macro_use]
+extern crate log;
+use log::{debug, info, warn, error};
+extern crate simple_logger;
+
 extern crate jack;
 use jack::{Client, Port, MidiIn, MidiOut, RawMidi, MidiWriter, ProcessHandler, ProcessScope};
 
@@ -12,6 +17,11 @@ extern crate rosc;
 use rosc::{OscPacket, OscMessage, OscMidiMessage, OscType, encoder, decoder};
 
 fn main() {
+	simple_logger::init().unwrap();
+	
+	jack::set_info_callback(|info| {debug!(target: "jack", "{}", info);});
+	jack::set_error_callback(|error| {warn!(target: "jack", "{}", error);});
+    
 	let matches = App::new(crate_name!())
 		.version(crate_version!())
 		.author(crate_authors!())
@@ -41,10 +51,10 @@ fn main() {
 	let send = matches.is_present("send");
 	let receive = matches.is_present("receive");
 	
-	let (client, _status) = Client::new(matches.value_of("name").unwrap(), jack::ClientOptions::NO_START_SERVER).unwrap();
+	let (client, _status) = Client::new(matches.value_of("name").unwrap(), jack::ClientOptions::NO_START_SERVER).expect("Could not connect to JACK server");
 	let count = matches.value_of("count").unwrap();
-	let input_ports: Vec<Port<MidiIn>> = (0..5).map(|i| client.register_port(&format!("input_{}", i), jack::MidiIn::default()).unwrap()).collect();
-	let output_ports: Vec<Port<MidiOut>> = (0..5).map(|i| client.register_port(&format!("output_{}", i), jack::MidiOut::default()).unwrap()).collect();
+	let input_ports: Vec<Port<MidiIn>> = (0..5).map(|i| client.register_port(&format!("input_{}", i), jack::MidiIn::default()).expect("Could not create MIDI input port")).collect();
+	let output_ports: Vec<Port<MidiOut>> = (0..5).map(|i| client.register_port(&format!("output_{}", i), jack::MidiOut::default()).expect("Could not create MIDI output port")).collect();
 
 	// Bi-directional channels
 	let (tx_input, rx_input) = mpsc::channel();
@@ -54,42 +64,48 @@ fn main() {
 		input_ports, output_ports, tx_input, rx_output
 	};
 	
-	let client = client.activate_async((), jack_handler).unwrap();
+	let client = client.activate_async((), jack_handler).expect("Could not start JACK client");
+	
+	// Create the sockets before starting background threads to take the whole application down on errors
+	let receive_socket = UdpSocket::bind("localhost:8953").expect("Can't create UDP socket");
+	let send_socket = UdpSocket::bind("localhost:0").expect("Can't create UDP socket");
 	
 	// Network receive handler
 	std::thread::spawn(move || {
-		let socket = UdpSocket::bind("localhost:8953").expect("Can't create UDP socket");
 		let mut buffer = vec![0;decoder::MTU];
 		loop {
-			let (read, _sender) = socket.recv_from(&mut buffer).unwrap();
-			println!("Received {:?}", decoder::decode(&mut buffer).unwrap());
-			
-			match decoder::decode(&mut buffer[..read]).unwrap() {
-				OscPacket::Message(OscMessage{addr, args}) => if addr == "/midi" {
-					if let Some(data) = args {
-						for osc_type in data.iter() {
-							if let OscType::Midi(midi_message) = osc_type {
-								tx_output.send(midi_message.clone());
+			match (receive_socket.recv_from(&mut buffer)) {
+				Ok((read, _sender)) => match decoder::decode(&mut buffer[..read]) {
+					Ok(OscPacket::Message(OscMessage{addr, args})) => if addr == "/midi" {
+						if let Some(data) = args {
+							for osc_type in data.iter() {
+								if let OscType::Midi(midi_message) = osc_type {
+									tx_output.send(midi_message.clone());
+								}
 							}
 						}
-					}
+					},
+					Ok(_) => (), // Simply ignore non-matching packets (Should we? Or should there be a warning?)
+					Err(e) => error!("Could not decode OSC packet: {:?}", e)
 				},
-				_ => ()
+				Err(e) => error!("Could not receive UDP packet: {}", e)
 			}
+			
 		}
 	});
 	
 	// Network send handler
-	let socket = UdpSocket::bind("0.0.0.0:0").expect("Can't create UDP socket");
-	socket.connect("localhost:8953").unwrap();
-	for message in rx_input.iter() {
-		println!("Sending {:?}", message);
-		let buffer = encoder::encode(&OscPacket::Message(OscMessage {
-			addr: "/midi".to_string(),
-			args: Some(vec![OscType::Midi(message)])
-		})).expect("Can't encode message");
-		println!("Sending {:?}", buffer);
-		socket.send(&buffer).unwrap();
+	std::thread::spawn(move || {
+		send_socket.connect("localhost:8953").unwrap();
+		for message in rx_input.iter() {
+			let buffer = encoder::encode(&OscPacket::Message(OscMessage {
+				addr: "/midi".to_string(),
+				args: Some(vec![OscType::Midi(message)])
+			})).expect("Can't encode message");
+			if let Err(e) = send_socket.send(&buffer) {
+				error!("Could not send UDP packet: {}", e);
+			}
+		}
 	}
 }
 
@@ -104,7 +120,6 @@ impl ProcessHandler for JackHandler {
 	fn process(&mut self, client: &Client, process_scope: &ProcessScope) -> jack::Control {
 		for port in (0..self.input_ports.len()) {
 			for event in self.input_ports[port].iter(process_scope) {
-				println!("Input {:?}", event);
 				self.tx_input.send(OscMidiMessage{
 					port: port as u8,
 					status: event.bytes[0],
@@ -121,12 +136,6 @@ impl ProcessHandler for JackHandler {
 		}
 		for port in (0..self.output_ports.len()) {
 			let mut writer = self.output_ports[port].writer(process_scope);
-			if (data[port].len() > 0) {
-			println!("Output {:?}", RawMidi {
-				time: 0,
-				bytes: &data[port]
-			});
-			}
 			writer.write(&RawMidi {
 				time: 0,
 				bytes: &data[port]
